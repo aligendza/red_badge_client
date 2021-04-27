e1b7466358228568286ed7c9b98f44462d315651
aligendza <73717510+aligendza@users.noreply.github.com>
2021-04-27 15:48:47 -0400

    new Navbar with Poses Dropdown menu

--- src/site/Navbar.tsx
+++ src/site/Navbar.tsx
@@ -1,64 +1,92 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Toolbar from '@material-ui/core/Toolbar'
 import { Link } from "react-router-dom";
-import { Button, Toolbar } from "@material-ui/core";
 import SwitchController from "./SwitchController";
-
-
 type AcceptedProps = {
-  updateToken: (newToken: string) => void;
-  updateRole: (newUserIsAdmin: string) => void;
-  clearToken: ()=> void;
-  sessionToken: any;
-  };
-  
-  export class Navbar extends Component<AcceptedProps, {}> {
+    updateToken: (newToken: string) => void;
+    updateRole: (newUserIsAdmin: string) => void;
+    clearToken: () => void;
+    sessionToken: any;
+};
+
+type ValueTypes = {
+    anchorEl: any;
+}
+
+export class Navbar extends Component<AcceptedProps, ValueTypes> {
     constructor(props: AcceptedProps) {
-      super(props);
-      this.state = {};
-      console.log(props);
+        super(props);
+        this.state = {
+            anchorEl: null
+        };
+        console.log(props);
     }
+
+    // export default function SimpleMenu() {
+    // [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
     render() {
         return (
-          <div className="mainNav">
-            <div id="navContainer"></div>
-            <h3>User Navbar</h3>
-            <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-              <Button style={{ margin: "1rem 3rem" }}>
-                <Link style={{ color: "#000000" }} to="/site/home">
-                  User Home
-                </Link>
-              </Button>
-    
-              <Button style={{ margin: "1rem 3rem" }}>
-                <Link style={{ color: "#000000" }} to="/components/CreatePose">
-                  Create a Pose
-                </Link>
-              </Button>
-    
-              <Button style={{ margin: "1rem 3rem" }}>
-                <Link style={{ color: "#000000" }} to="/components/CreateSequence">
-                  Create a Sequence
-                </Link>
-              </Button>
-    
-              <Button style={{ margin: "1rem 3rem" }}>
-                <Link style={{ color: "#000000" }} to="/components/MySequences">
-                  My Sequences
-                </Link>
-              </Button>
-    
-              <Button
-                style={{ margin: "1rem 3rem" }}
-                onClick={this.props.clearToken}>
-                <Link style={{ color: "#000000" }} to="/home">
-                  Logout
-                </Link>
-              </Button>
-              {console.log("Nav Footer")}
-            </Toolbar>
-            <SwitchController updateToken={this.props.updateToken} updateUserRole={this.props.updateRole} sessionToken={this.props.sessionToken} />
-          </div>
+            <div>
+                <div>
+                    <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+                        <Button style={{ margin: "1rem 3rem" }}>
+                            <Link style={{ color: "#000000" }} to="/site/home">
+                                User Home
+                            </Link>
+                        </Button>
+                        <Button style={{ margin: "1rem 3rem" }} aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+                            Poses
+                        </Button>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={this.state.anchorEl}
+                            keepMounted
+                            open={Boolean(this.state.anchorEl)}
+                            onClose={this.handleClose}>
+                            <MenuItem onClick={this.handleClose}>
+                                <Link style={{ color: "#000000" }} to="/components/CreatePose">
+                                    Create a Pose
+                                </Link>
+                            </MenuItem>
+                            <MenuItem onClick={this.handleClose}>
+                                <Link style={{ color: "#000000" }} to="/components/EditPose">
+                                    Edit a Pose
+                                </Link>
+                            </MenuItem>
+                            <MenuItem onClick={this.handleClose}>
+                                <Link style={{ color: "#000000" }} to="/components/DeletePose">
+                                    Delete a Pose
+                                </Link>
+                            </MenuItem>
+                            <MenuItem onClick={this.handleClose}>
+                                <Link style={{ color: "#000000" }} to="/components/GetAllPoses">
+                                    Get All Poses
+                                </Link>
+                            </MenuItem>
+                        </Menu>
+                        <Button
+                            style={{ margin: "1rem 3rem" }}
+                            onClick={this.props.clearToken}>
+                            <Link style={{ color: "#000000" }} to="/home">
+                                Logout
+                            </Link>
+                        </Button>
+                    </Toolbar>
+                    <SwitchController updateToken={this.props.updateToken} updateUserRole={this.props.updateRole} sessionToken={this.props.sessionToken} />
+                </div>
+            </div>
         );
-      }
     }
-    export default Navbar;
+}
+export default Navbar;
---
+++ src/site/NavbarOldWorking.tsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Toolbar } from "@material-ui/core";
+import SwitchController from "./SwitchController";
+
+
+type AcceptedProps = {
+  updateToken: (newToken: string) => void;
+  updateRole: (newUserIsAdmin: string) => void;
+  clearToken: ()=> void;
+  sessionToken: any;
+  };
+  
+  export class Navbar extends Component<AcceptedProps, {}> {
+    constructor(props: AcceptedProps) {
+      super(props);
+      this.state = {};
+      console.log(props);
+    }
+    render() {
+        return (
+          <div className="mainNav">
+            <div id="navContainer"></div>
+            <h3>User Navbar</h3>
+            <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+              <Button style={{ margin: "1rem 3rem" }}>
+                <Link style={{ color: "#000000" }} to="/site/home">
+                  User Home
+                </Link>
+              </Button>
+    
+              <Button style={{ margin: "1rem 3rem" }}>
+                <Link style={{ color: "#000000" }} to="/components/CreatePose">
+                  Create a Pose
+                </Link>
+              </Button>
+    
+              <Button style={{ margin: "1rem 3rem" }}>
+                <Link style={{ color: "#000000" }} to="/components/CreateSequence">
+                  Create a Sequence
+                </Link>
+              </Button>
+    
+              <Button style={{ margin: "1rem 3rem" }}>
+                <Link style={{ color: "#000000" }} to="/components/MySequences">
+                  My Sequences
+                </Link>
+              </Button>
+    
+              <Button
+                style={{ margin: "1rem 3rem" }}
+                onClick={this.props.clearToken}>
+                <Link style={{ color: "#000000" }} to="/home">
+                  Logout
+                </Link>
+              </Button>
+              {/* {console.log("Nav Footer")} */}
+            </Toolbar>
+            <SwitchController updateToken={this.props.updateToken} updateUserRole={this.props.updateRole} sessionToken={this.props.sessionToken} />
+          </div>
+        );
+      }
+    }
+export default Navbar;
