c583d8161d65d2776228b26ba3b940170b5d11d7
aligendza <73717510+aligendza@users.noreply.github.com>
2021-04-28 09:59:40 -0400

    drop down menus working

--- src/site/Navbar.tsx
+++ src/site/Navbar.tsx
@@ -14,13 +14,15 @@ type AcceptedProps = {
 
 type ValueTypes = {
     anchorEl: any;
+    anchorEl2: any;
 }
 
 export class Navbar extends Component<AcceptedProps, ValueTypes> {
     constructor(props: AcceptedProps) {
         super(props);
         this.state = {
-            anchorEl: null
+            anchorEl: null,
+            anchorEl2: null
         };
         console.log(props);
     }
@@ -31,8 +33,14 @@ export class Navbar extends Component<AcceptedProps, ValueTypes> {
     handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         this.setState({ anchorEl: event.currentTarget });
     };
+    handleClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {
+        this.setState({ anchorEl2: event.currentTarget });
+    };
 
     handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+    handleClose2 = () => {
         this.setState({ anchorEl: null });
     };
     render() {
@@ -77,31 +85,31 @@ export class Navbar extends Component<AcceptedProps, ValueTypes> {
                             </MenuItem>
                         </Menu>
                         {/* Sequences Menu */}
-                        {/* <Button style={{ margin: "1rem 3rem" }} aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+                        <Button style={{ margin: "1rem 3rem" }} aria-controls="simple-menu2" aria-haspopup="true" onClick={this.handleClick2}>
                             Sequences
                         </Button>
                         <Menu
-                            id="simple-menu"
-                            anchorEl={this.state.anchorEl}
+                            id="simple-menu2"
+                            anchorEl={this.state.anchorEl2}
                             keepMounted
-                            open={Boolean(this.state.anchorEl)}
-                            onClose={this.handleClose}>
-                            <MenuItem onClick={this.handleClose}>
+                            open={Boolean(this.state.anchorEl2)}
+                            onClose={this.handleClose2}>
+                            <MenuItem onClick={this.handleClose2}>
                                 <Link style={{ color: "#000000" }} to="/components/CreateSequence">
                                     Create a Sequence
                                 </Link>
                             </MenuItem>
-                            <MenuItem onClick={this.handleClose}>
+                            <MenuItem onClick={this.handleClose2}>
                                 <Link style={{ color: "#000000" }} to="/components/EditSequence">
                                     Edit a Sequence
                                 </Link>
                             </MenuItem>
-                            <MenuItem onClick={this.handleClose}>
+                            <MenuItem onClick={this.handleClose2}>
                                 <Link style={{ color: "#000000" }} to="/components/DeleteSequence">
                                     Delete a Sequence
                                 </Link>
                             </MenuItem>
-                            <MenuItem onClick={this.handleClose}>
+                            <MenuItem onClick={this.handleClose2}>
                                 <Link style={{ color: "#000000" }} to="/components/GetAllSequences">
                                     Get All Sequences
                                 </Link>
@@ -113,7 +121,7 @@ export class Navbar extends Component<AcceptedProps, ValueTypes> {
                             <Link style={{ color: "#000000" }} to="/home">
                                 Logout
                             </Link>
-                        </Button> */}
+                        </Button>
                     </Toolbar>
                     <SwitchController updateToken={this.props.updateToken} updateUserRole={this.props.updateRole} sessionToken={this.props.sessionToken} />
                 </div>
