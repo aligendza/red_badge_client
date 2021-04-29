5871d1b7b2aafe5d285b122ff01da450f316a8c1
aligendza <73717510+aligendza@users.noreply.github.com>
2021-04-29 17:38:07 -0400

    seq files added not working

--- src/components/GetAllPoses.tsx
+++ src/components/GetAllPoses.tsx
@@ -1,124 +1,138 @@
-import React, { Component } from 'react';
-import APIURL from '../helpers/environment';
+import React, { Component } from "react";
+import APIURL from "../helpers/environment";
 // import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 // import { Button, TextField } from '@material-ui/core';
-import FormControl from '@material-ui/core/FormControl';
+import FormControl from "@material-ui/core/FormControl";
 // import { YogaPose } from '../Interfaces';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
-import CardItemDisplay from './PoseCard'
-import ViewPose from "./ViewPoses"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import CardItemDisplay from "./notUsed/PoseCard";
+import ViewPose from "./ViewPoses";
 
 type AcceptedProps = {
-    // updateToken: (newToken: string) => void;  // string | null 
-    sessionToken: any
-    // updateRole: (newUserIsAdmin: string) => void;
-    // YogaPose: [];
+  // updateToken: (newToken: string) => void;  // string | null
+  sessionToken: any;
+  // updateRole: (newUserIsAdmin: string) => void;
+  // YogaPose: [];
 };
 
 type PoseDataState = {
-    id: number;
-    nameEng: string;
-    nameSans: string;
-    imgUrl: string;
-    poseCat: string;
-    poses: []
-}
+  id: number;
+  nameEng: string;
+  nameSans: string;
+  imgUrl: string;
+  poseCat: string;
+  poses: [];
+};
 
-type styleState= {
-    style: any,
-    index: any
-}
+type styleState = {
+  style: any;
+  index: any;
+};
 
 class renderRow extends Component<ListChildComponentProps, styleState> {
-    constructor(props: ListChildComponentProps) {
-        super(props);
-        this.state={
-            index:this.props.index,
-            style:this.props.style
-        }
-    }
+  constructor(props: ListChildComponentProps) {
+    super(props);
+    this.state = {
+      index: this.props.index,
+      style: this.props.style,
+    };
+  }
 
-render(){
+  render() {
     return (
-        <ListItem button style={this.state.style} key={this.state.index}>
-            <ListItemText primary={`Item ${this.state.index + 1}`} />
-        </ListItem>
+      <ListItem button style={this.state.style} key={this.state.index}>
+        <ListItemText primary={`Item ${this.state.index + 1}`} />
+      </ListItem>
     );
-    }
+  }
 }
 
-export default class GetAllPoses extends Component<AcceptedProps, PoseDataState> {
-    constructor(props: AcceptedProps) {
-        super(props);
-        this.state = {
-            id: 0,
-            nameEng: '',
-            nameSans: '',
-            imgUrl: '',
-            poseCat: '',
-            poses: []
-        }
+export default class GetAllPoses extends Component<
+  AcceptedProps,
+  PoseDataState
+> {
+  constructor(props: AcceptedProps) {
+    super(props);
+    this.state = {
+      id: 0,
+      nameEng: "",
+      nameSans: "",
+      imgUrl: "",
+      poseCat: "",
+      poses: [],
     };
-    token: string | null = localStorage.getItem("token");
+  }
+  token: string | null = localStorage.getItem("token");
 
-    handleSubmit = () => {
-
-        if (this.props.sessionToken) {
-            // e.preventDefault();
-            // fetch("http://localhost:3000/pose/create", {
-                console.log (APIURL,this.props.sessionToken,this.token, this.state.id, this.state.nameEng, this.state.nameSans, this.state.imgUrl, this.state.poseCat);
-            fetch(`${APIURL}/pose/`, {
-                method: 'GET',
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    Authorization: this.props.sessionToken,
-                }),
-
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    console.log(data, this.props.sessionToken, this.token);
-                    this.setState({poses: data})
-                })
-                .catch((err) => console.log(err));
-        }
-    };
-    // classes = useStyles();
-    componentDidMount(){
-        this.handleSubmit()
+  handleSubmit = () => {
+    if (this.props.sessionToken) {
+      // e.preventDefault();
+      // fetch("http://localhost:3000/pose/create", {
+      console.log(
+        APIURL,
+        this.props.sessionToken,
+        this.token,
+        this.state.id,
+        this.state.nameEng,
+        this.state.nameSans,
+        this.state.imgUrl,
+        this.state.poseCat
+      );
+      fetch(`${APIURL}/pose/`, {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: this.props.sessionToken,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data, this.props.sessionToken, this.token);
+          this.setState({ poses: data });
+        })
+        .catch((err) => console.log(err));
     }
+  };
+  // classes = useStyles();
+  componentDidMount() {
+    this.handleSubmit();
+  }
 
-    render() {
-        return (
-            <div 
-            // className={this.classes.root}>
-                >
-                <h2>All My Poses</h2>
-                <ViewPose sessionToken={this.props.sessionToken} poses={this.state.poses} />
-                {/* <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
+  render() {
+    return (
+      <div
+      // className={this.classes.root}>
+      >
+        <h2>All My Poses</h2>
+        <ViewPose
+          sessionToken={this.props.sessionToken}
+          poses={this.state.poses}
+        />
+        {/* <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
                     {renderRow}
                 </FixedSizeList>
                 <br />
                 <Button variant='contained' onClick={this.handleSubmit}>
                     Get All My Poses
                     </Button> */}
-
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 // export default function VirtualizedList() {
-    // const classes = useStyles();
+// const classes = useStyles();
 
 //     return (
 //       <div className={classes.root}>
-{/* <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
+{
+  /* <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
     {renderRow}
-</FixedSizeList> */}
+</FixedSizeList> */
+}
 //       </div>
 //     );
 //   }
---
+++ src/components/SeqCardCreate.tsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+type acceptedProps = {
+  sessionToken: any;
+  pose: any;
+  index: any;
+};
+
+export default class PosesCard extends Component<acceptedProps, {}> {
+  constructor(props: acceptedProps) {
+    super(props);
+    this.state = {};
+    console.log(this.props.pose);
+  }
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              //   image={props.imgUrl}
+              title={this.props.pose.nameEng}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {/* Lizard */}
+                {this.props.pose.nameEng}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {/* Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica */}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            {/* <Button size="small" color="primary">
+              Delete
+            </Button> */}
+            <Button size="small" color="primary">
+              Add Pose
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
